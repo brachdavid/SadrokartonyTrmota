104a68fcae390fb88614a967054a83239c1de632
brachdavid <153853659+brachdavid@users.noreply.github.com>
2025-03-01 22:41:41 +0100

    Add a functional contact form.
    - Add a folder "Models" and a class "EmailRequest".
    - Add a contact form built on the NuGet MailKit package.
    - Add consent to process personal data.

---
+++ Models/EmailRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace SadrokartonyTrmota.Models
+{
+    public class EmailRequest
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Message { get; set; }
+
+    }
+}
